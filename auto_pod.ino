2996f237f7b36ad29e5d34aabdf2495b3b8b795b
Jacob Atkins <jacob.atkins@silverneedlesoft.com>
2012-04-21 01:05:09 -0400

    Compiles, added lcd code

--- auto_pod.ino
+++ auto_pod.ino
@@ -1,4 +1,6 @@
 #include <Keypad.h>
+#include <Servo.h>
+#include <LiquidCrystal.h>
 
 const byte ROWS = 4;
 const byte COLS = 3;
@@ -10,7 +12,19 @@ char nav_keys[ROWS][COLS] = {
   {'*','0','#'} 
 };
 
-byte row_pins[rows] = {5, 4, 3, 2};
-byte col_pins[cols] = {8, 7, 6};
+LiquidCrystal lcd(12, 11, 10, 9, 13, 14);
 
-Keypad keypad = Keypad( makeKeymap(keys), row_pins, col_pins, rows, cols );
+byte row_pins[ROWS] = {5, 4, 3, 2};
+byte col_pins[COLS] = {8, 7, 6};
+
+Keypad keypad = Keypad( makeKeymap(nav_keys), row_pins, col_pins, ROWS, COLS );
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("hello, world"); 
+}
+
+void loop() {
+  lcd.setCursor(0,1);
+  lcd.print(millis()/1000); 
+}
