183b104db9b2c5d6494383de7e89d8d7c5564739
Jacob Atkins <jacob.atkins@silverneedlesoft.com>
2012-05-06 13:06:25 -0400

    GPS working, pretty much rebuild all the code

--- auto_pod.ino
+++ auto_pod.ino
@@ -1,45 +1,82 @@
-#include <Keypad.h>
-#include <Servo.h>
 #include <LiquidCrystal.h>
+#include <SD.h>
 
-const byte ROWS = 4;
-const byte COLS = 3;
+const int chipSelect = 53;
+const char gpsTag[] = "$GPRMC";
 
-char nav_keys[ROWS][COLS] = {
-  {'1','2','3'},
-  {'4','5','6'},
-  {'7','8','9'},
-  {'*','0','#'} 
-};
-
-LiquidCrystal lcd(12, 11, 10, 9, 13, 14);
-
-byte row_pins[ROWS] = {5, 4, 3, 2};
-byte col_pins[COLS] = {8, 7, 6};
-
-Keypad keypad = Keypad( makeKeymap(nav_keys), row_pins, col_pins, ROWS, COLS );
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 
 void setup() {
-  lcd.begin(16, 2);
-  lcd.print("hello, world"); 
+  Serial.begin(9600);
+  Serial1.begin(9600);
+ 
+ lcd.begin(16,2);
+ 
+  delay(200);
+ Serial1.println("$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29");
+ delay(200);
+ Serial1.println("$PMTK220,1000*1F");
+ 
+ pinMode(53, OUTPUT);
+ if(!SD.begin(chipSelect)) {
+   lcd.print("SD failed!");
+   return;
+ }
+ lcd.print("SD initialized");
+  
 }
 
 void loop() {
-  lcd.setCursor(0,1);
-  lcd.print(millis()/1000); 
-}
-
-int start() {
-  lcd.setCursor(0,0);
-  lcd.print("Set base to 0, 0");
-  lcd.setCursor(0,1);
-  lcd.print("Press * when done");
-  char key = keypad.getKey();
-  if(key == '*'){
-    return 1;
-  }
+  poll_gps();
+  delay(1000);
 }
 
 void poll_gps() {
-   
+  String gps = "";
+  
+  while(Serial1.available()) {
+    gps += String((char)Serial1.read());
+  }
+  
+  if(check_gps(gps)==1){
+    if(gps[18]=='A'){
+      lcd.clear();
+      lcd.print("Time: " + time(gps.substring(7, 13)));
+      lcd.setCursor(0,1);
+      lcd.print("Date: " + date(gps.substring(57, 63)));
+    } else {
+      lcd.clear();
+      lcd.print("No Lock!");
+      lcd.setCursor(9, 0);
+      lcd.print(gps.substring(7, 13));
+      lcd.setCursor(0,1);
+      lcd.print(gps.substring(57, 63));
+    } 
+  }
+}
+
+int check_gps(String data){
+  for(int i=0;i<6;i++){
+    if(data[i]!=gpsTag[i]){
+      return 0;
+    }
+   }
+  return 1;
+}
+
+String date(String gps_date) {
+  String day   = gps_date.substring(0,2);
+  String month = gps_date.substring(2,4);
+  String year  = gps_date.substring(4,6);
+  
+  String date  = String(month + "-" + day + "-" + "20" + year);
+  return date; 
+}
+
+String time(String gps_time) {
+  String hour    = gps_time.substring(0,2);
+  String miniute = gps_time.substring(2,4);
+  String second  = gps_time.substring(4,6);
+  String time    = String(hour + ":" + miniute + ":" + second);
+  return time;
 }
